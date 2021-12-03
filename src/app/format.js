4068ca1be17afa5330483168e1f321372ad6d72e
MorganeLM <morganelemarquer@lilo.org>
2021-12-03 14:23:31 +0100

    correct date error

--- src/app/format.js
+++ src/app/format.js
@@ -1,12 +1,17 @@
 export const formatDate = (dateStr) => {
   const date = new Date(dateStr)
-  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
-  const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
-  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
-  const month = mo.charAt(0).toUpperCase() + mo.slice(1)
-  return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+  // other bug when dateStr is not a valid date (Firefox error: ""RangeError: date value is not finite in DateTimeFormat.format()")
+  const regEx = /^\d{4}-\d{2}-\d{2}$/;
+  if(Number.isNaN(dateStr) || dateStr === '' || !dateStr.match(regEx)){
+    return `Date non valide`
+  } else {
+    const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
+    const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
+    const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+    const month = mo.charAt(0).toUpperCase() + mo.slice(1)
+    return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+  }
 }
- 
 export const formatStatus = (status) => {
   switch (status) {
     case "pending":
